b07da991e2f3687b9d588a53b523a6b1cf1b26c5
Mysyka <61739403+Mysyka@users.noreply.github.com>
2020-03-03 18:35:48 +0300

    1

--- 1_1.py
+++ 1_1.py
@@ -2,8 +2,8 @@ import requests
 
 urls = ['http://yandex.ru', 'http://google.ru', 'http://mail.ru',
        'http://rbc.ru', 'http://rambler.ru', 'http://yahoo.com', 'http://megafon.ru']
-for n in range(101):
-    for i in urls:
+for i in urls:
+    for n in range(101):
         a = requests.get(i).status_code
         if a!=200:
             print(a)
