9c1f4868e50c821133874f59a5329a3b5aa9661a
Mysyka <61739403+Mysyka@users.noreply.github.com>
2020-03-03 20:45:21 +0300

    1

--- ping.py
+++ ping.py
@@ -1,12 +1,17 @@
-import os
+import requests
+hostname = '10.200.8.'
+i = '200'
 
-hostname = "google.com"  # example
-response = os.system("ping -c 1 " + hostname)
+hostname = hostname+i
+i = 'i+1'
+
+host = 'http://hostname'
+# hostname = "google.com"  # example
+response = requests.get(host).status_code
 
 # and then check the response...
-if response == 0:
-    print
-    hostname, 'is up!'
-else:
-    print
-    hostname, 'is down!'
+#if response == 0:
+#    print(hostname, 'is up!')
+
+#else:
+#    print(hostname, 'is down!')
---
+++ sdfsdf.py
@@ -0,0 +1,5 @@
+import requests
+r = requests.get('10.200.8.200'.)
+print(r.text)
+print(r)
+print(bool(r))
