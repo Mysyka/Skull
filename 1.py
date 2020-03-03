a9cec67ea08aa348ec681ab564d4119b3eaaf9cd
Mysyka <61739403+Mysyka@users.noreply.github.com>
2020-03-03 18:13:08 +0300

    1

---
+++ 1.py
@@ -0,0 +1,31 @@
+import requests
+
+urls = {'url0': 'http://yandex.ru',
+        'url1': 'http://google.ru',
+        'url2': 'http://mail.ru',
+        'url3': 'http://rbc.ru',
+        'url4': 'http://rambler.ru',
+        'url5': 'http://yahoo.com'}
+
+for i in range(100):
+    print(
+    requests.get(urls['url0']).status_code,'\n',
+    requests.get(urls['url1']).status_code,'\n',
+    requests.get(urls['url2']).status_code,'\n',
+    requests.get(urls['url3']).status_code,'\n',
+    requests.get(urls['url4']).status_code,'\n',
+    requests.get(urls['url5']).status_code
+    )
+
+
+#print(requests.get('http://yandex.ru').status_code)
+#get_stat = requests.get(url['url0']).status_code
+
+#r = requests.get(url0)
+#k = requests.get('http://google.ru')
+#c = requests.get('http://mail.ru')
+#f = requests.get('http://rbc.ru')
+#l = requests.get('http://rambler.ru')
+#p = requests.get('http://yahoo.com')
+
+
---
+++ 1_1.py
@@ -0,0 +1,9 @@
+import requests
+
+urls = ['http://yandex.ru', 'http://google.ru', 'http://mail.ru',
+       'http://rbc.ru', 'http://rambler.ru', 'http://yahoo.com', 'http://megafon.ru']
+for n in range(101):
+    for i in urls:
+        a = requests.get(i).status_code
+        if a!=200:
+            print(a)
