e7fe183e0403bf5e2c5e9a6e0cce6c8b53e9e924
Mysyka <catisblack@gmail.com>
2020-03-04 12:34:51 +0300

    from x5_comp

--- 123/ss_demo.py
+++ 123/ss_demo.py
@@ -1,4 +1,3 @@
 alphabet2 = '01'
 alphabet10 = '0123456789'
 alphabet16 = '0123456789ABCDEF'
-123123
--- 1_1.py
+++ 1_1.py
@@ -1,9 +1,12 @@
 import requests
 
+import urllib3
+urllib3.disable_warnings()
+
 urls = ['http://yandex.ru', 'http://google.ru', 'http://mail.ru',
        'http://rbc.ru', 'http://rambler.ru', 'http://yahoo.com', 'http://megafon.ru']
 for i in urls:
-    for n in range(101):
-        a = requests.get(i).status_code
+    for n in range(2):
+        a = requests.get(i, verify=False).status_code
         if a!=200:
             print(a)
