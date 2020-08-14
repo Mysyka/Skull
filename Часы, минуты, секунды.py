40e0f3b16146aba1533672aa99a0d7ca2c28cdab
Mysyka <catisblack@gmail.com>
2020-08-14 04:00:20 +0300

    from x5_compfgfdgd

--- Часы, минуты, секунды.py
+++ Часы, минуты, секунды.py
@@ -1,16 +1,18 @@
-
 H = 'час'
 M = 'минут'
 S = 'секунд'
 
-a=int(input('Введите количество секунд:'))
-h=a//3600
-m=(a//60)%60
-s=a%60
+#перевод секунд в минуты и часы
+a = int(input('Введите количество секунд:'))
+h = a//3600
+m = (a//60)%60
+s = a%60
+
+#окончания
 if 2 <= h <= 4:
     H = H + 'а'
 if h >= 5 or h == 0:
-    H = H+ 'ов'
+    H = H + 'ов'
 if m == 1:
     M = M + 'а'
 if 2 <= m <= 4:
@@ -19,13 +21,17 @@ if s == 1:
     S = S + 'а'
 if 2 <= s <= 4:
     S = S + 'ы'
-
-if h == 0:
-    print(m, M, s, S);
-
-if m == 0:
-    print(h, H, s, S);
-
-if s == 0:
-    print(h, H, m, M);
-
+print(h, H, m, M, s, S)
+#проверка на нули/ но тогда вывод работает криво
+#if h == 0:
+#    print(m, M, s, S)
+#else:
+#    print(h, H, m, M, s, S)
+#if m == 0:
+#    print(h, H, s, S)
+#else:
+#    print(h, H, m, M, s, S)
+#if s == 0:
+#    print(h, H, m, M)
+#else:
+#    print(h, H, m, M, s, S)
