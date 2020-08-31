dce848ca6426d5870bc33bf27cbbb15305b0152e
Mysyka <catisblack@gmail.com>
2020-08-31 11:00:59 +0300

    new

--- Школа Х5/Урок 1/comapre.py
+++ Школа Х5/Урок 1/comapre.py
@@ -2,33 +2,15 @@ print(2 == 2)
 print(2 == 2.0)
 print(3 == 0)
 
-
-
 print('a' == 'a')
 print('abc' == 'abc')
 
-
-
-
-print(2 == 2 is True)
 print(None is None)
-
-
-
-
-
 
 first_number = 256
 second_number = 256
 
 print(first_number is second_number)
-
-
-
-
-
-
-
 
 first_number = 257
 second_number = 257
